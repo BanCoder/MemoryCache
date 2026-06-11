2e52058adc6a109a1cda52bc22971477dd3b9545
Vladimir Belskiy <belskiy548@gmail.com>
2026-06-11 17:43:44 +0300

    Change swagger settings

--- MemoryCache.Web/Program.cs
+++ MemoryCache.Web/Program.cs
@@ -23,11 +23,8 @@ builder.Services.AddBusinessLogic();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Use(async (context, next) =>
