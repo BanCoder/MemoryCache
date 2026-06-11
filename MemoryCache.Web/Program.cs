ef400971fd74b6ca528f6ae4d6627606a66a3fe3
Vladimir Belskiy <belskiy548@gmail.com>
2026-06-11 18:51:03 +0300

    test new connection string

--- MemoryCache.Web/Program.cs
+++ MemoryCache.Web/Program.cs
@@ -1,6 +1,7 @@
 using BusinessLogic;
 using DataAccess;
 using Serilog;
+using Microsoft.EntityFrameworkCore;
 
 Log.Logger = new LoggerConfiguration()
 	.MinimumLevel.Information()
@@ -12,13 +13,20 @@ Log.Logger = new LoggerConfiguration()
 	.CreateLogger(); 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog();
-builder.Services.AddMemoryCache(); 
+builder.Services.AddMemoryCache();
 //builder.Services.AddStackExchangeRedisCache(options =>
 //{
 //	options.Configuration = "localhost:6379";
 //	options.InstanceName = "ProductApp_"; 
 //});
-builder.Services.AddDataAccess(builder.Configuration);
+var config = new ConfigurationBuilder()
+	.AddInMemoryCollection(new Dictionary<string, string?>
+	{
+		["ConnectionStrings:DefaultConnection"] = "Host=dpg-d8lc9028qa3s73a1lavg-a.oregon-postgres.render.com;Port=5432;Database=cachedb_y461;Username=cachedb_y461_user;Password=0enjGJ8NoSl0SvlSiRw0CLPccgZBNoh9;SSL Mode=Require;Trust Server Certificate=true"
+	})
+	.Build();
+
+builder.Services.AddDataAccess(config);
 builder.Services.AddBusinessLogic();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
